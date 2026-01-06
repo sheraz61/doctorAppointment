1338aa4e978d68e5dcb181407f58ff6d251172ed
sheraz61 <hsheraz271@gmail.com>
2026-01-06 21:26:27 +0500

    create folder structure  and define routes also download useable assets....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointment from './pages/MyAppointment'
+import Appointment from './pages/Appointment'
 
 function App() {
   return (
-    <div className='text-6xl'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointments' element={<MyAppointment/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
+    </div>
   )
 }
 
