aa01d84dc35678d0ad66274c93f9f7c5bb0013a1
sheraz61 <hsheraz271@gmail.com>
2026-01-07 21:42:13 +0500

    add footer and update font style add outfit font for the UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointment from './pages/MyAppointment'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path='/my-appointments' element={<MyAppointment/>}/>
         <Route path='/appointment/:docId' element={<Appointment/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
