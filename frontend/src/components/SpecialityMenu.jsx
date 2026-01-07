0faa0cd79fe981f2776d31262dd6abf74efce379
sheraz61 <hsheraz271@gmail.com>
2026-01-07 21:11:08 +0500

    some ui scroll fixes and also add context for display doctors data and complete top doctor component...

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -3,7 +3,7 @@ import { specialityData } from '../assets/assets'
 import { Link } from 'react-router-dom'
 function SpecialityMenu() {
     return (
-        <div id='#speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
+        <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
             <h1 className='text-3xl font-medium '>Find by Speciality</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free. </p>
             <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
