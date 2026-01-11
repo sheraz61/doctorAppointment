8b09d6095cd74bd169c943f84d62ae2e0c826416
sheraz61 <hsheraz271@gmail.com>
2026-01-11 22:23:14 +0500

    create admin folder and create admin and doctor login... also create add doctor page connect backend of login admin and add doctor controller ..

---
+++ admin/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import Login from './pages/login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard'
+import AllAppointment from './pages/Admin/AllAppointment';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorList from './pages/Admin/DoctorList';
+function App() {
+  const {aToken} =useContext(AdminContext)
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+       <ToastContainer/>
+       <Navbar/>
+       <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/> }/>
+          <Route path='/all-appointment' element={<AllAppointment/> }/>
+          <Route path='/add-doctor' element={<AddDoctor/> }/>
+          <Route path='/doctor-list' element={<DoctorList/> }/>
+        </Routes>
+       </div>
+    </div>
+  ) : (
+    <>
+    <Login/>
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
